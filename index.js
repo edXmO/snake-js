52e2bcfffb7aa28db7468e048c686a71c0104cce
Eduardo <ex.romeroma@gmail.com>
2021-07-04 18:43:44 +0200

    movement across borders

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const WIDTH = 400;
 const HEIGHT = 400;
 const RES = 20;
 
-let maxFps = 400;
+let maxFps = 120;
 let timestamp;
 let frameTime;
 
@@ -29,8 +29,8 @@ class Cell {
         ctx.beginPath();
         ctx.lineWidth = 1;
         ctx.moveTo(this.x, this.y);
-        ctx.rect(this.x, this.y, this.w, this.h);
-        ctx.stroke();
+        ctx.fillStyle = "#565656";
+        ctx.fillRect(this.x, this.y, this.w, this.h);
     }
 }
 
@@ -48,29 +48,47 @@ class Snake {
         return this.x + this.y / RES;
     }
 
-    // Nota para que siga en una direccion concreta en cada
-    // momento podemos hacer un while que mantenga la suma de los indices
-    // en todo momento atendiendo a la ultima key presionada
-    // y atentos a dar la vuelta cuando lleguemos al borde
-
-    update(key = this.dir) {
+    update(key) {
         this.dir = key;
         switch (this.dir) {
             case "ArrowUp":
-                this.x = grid[snake.getIndex() - 1].x;
-                this.y = grid[snake.getIndex() - 1].y;
-                break;
-            case "ArrowRight":
-                this.x = grid[snake.getIndex() + RES].x;
-                this.y = grid[snake.getIndex() + RES].y;    
+                if(grid[snake.getIndex()].y  === 0){
+                    this.x = grid[snake.getIndex() + RES - 1].x;
+                    this.y = grid[snake.getIndex() + RES - 1].y;
+                } else {
+                    this.x = grid[snake.getIndex() - 1].x;
+                    this.y = grid[snake.getIndex() - 1].y;
+                }
                 break;
             case "ArrowDown":
-                this.x = grid[snake.getIndex() + 1].x;
-                this.y = grid[snake.getIndex() + 1].y;
+                if(grid[snake.getIndex()].y  === HEIGHT - RES){
+                    this.x = grid[snake.getIndex() - RES + 1].x;
+                    this.y = grid[snake.getIndex() - RES + 1].y;
+                }
+                else {
+                    this.x = grid[snake.getIndex() + 1].x;
+                    this.y = grid[snake.getIndex() + 1].y;
+                }
+                break;
+            case "ArrowRight":               
+                if(grid[snake.getIndex()].x === WIDTH - RES){
+                    this.x = grid[snake.getIndex() - ((RES - 1) * ROWS)].x;
+                    this.y = grid[snake.getIndex() - ((RES - 1) * ROWS)].y;
+                }
+                else {
+                    this.x = grid[snake.getIndex() + RES].x;
+                    this.y = grid[snake.getIndex() + RES].y;    
+                }
                 break;
             case "ArrowLeft":
-                this.x = grid[snake.getIndex() - RES].x;
-                this.y = grid[snake.getIndex() - RES].y;
+                if(grid[snake.getIndex()].x === 0){
+                    this.x = grid[snake.getIndex() + ((RES - 1) * ROWS)].x;
+                    this.y = grid[snake.getIndex() + ((RES - 1) * ROWS)].y;
+                }
+                else {
+                    this.x = grid[snake.getIndex() - RES].x;
+                    this.y = grid[snake.getIndex() - RES].y;
+                }
                 break;
         }
     }
@@ -134,13 +152,13 @@ snake.show();
 
 window.addEventListener('keydown', (e) => {
     const { key } = e;
+    
     snake.update(key);
-    snake.show();
+
+
 }, true);
 
-
 // intervalID = setInterval(() => placeRandomFood(), 5000);
-
 
 const tick = (timestamp) => {
     if (timestamp < frameTime + (1000 / maxFps)) {
@@ -149,9 +167,9 @@ const tick = (timestamp) => {
 
     frameTime = timestamp;
 
-    snake.show();
     drawGrid();
-    snake.update();
+    snake.show();
+
     // placeRandomFood();
 
     animationFrameID = requestAnimationFrame(tick)
